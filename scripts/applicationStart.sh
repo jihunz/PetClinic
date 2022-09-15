c025555f3f733278b895418c1730311544c8fe06
jihunz <thespringsong@gmail.com>
2022-09-15 10:55:00 +0900

    build: 배포 스크립트 수정

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # 애플리케이션의 배포 파일 폴더로 이동하여 애플리케이션을 실행한다
-sudo su
 cd /
 cd ../home/ec2-user/Petclinic/build/libs
-java -jar spring-petclinic-2.6.0.jar
+sudo java -jar spring-petclinic-2.6.0.jar
