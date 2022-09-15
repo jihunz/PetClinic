0729bcb6f8f1ac6f0e031e1ad3913103986b0672
jihunz <thespringsong@gmail.com>
2022-09-15 13:45:16 +0900

    build: 배포 테스트14 again

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 애플리케이션의 배포 파일 폴더로 이동하여 애플리케이션을 실행한다
+# 명령어의 표준출력을 버리고(> /dev/null 2>&1), 백그라운드(&)로 실행한다.
 cd /
 cd ../home/ec2-user/Petclinic/build/libs
-sudo java -jar spring-petclinic-2.6.0.jar
-echo "server run completed"
+sudo java -jar spring-petclinic-2.6.0.jar > /dev/null 2>&1 &
